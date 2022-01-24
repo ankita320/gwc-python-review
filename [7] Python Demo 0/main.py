40a7e3fef6ca275f6bbeb0b10a9c769f73861f9a
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-24 14:43:19 -0500

    Create main.py

---
+++ [7] Python Demo 0/main.py
@@ -0,0 +1,16 @@
+print("Welcome to the Kat-Chat Box!")
+print("-" * 40)
+
+name = input("What's your name? ")
+print("Nice to finally meet you, " + name + ".\n")
+
+print("I hear your winter break is coming up...")
+coolThing = input("What are you most excited for during the break? ")
+print("I love " + coolThing + "!\nI wish i had breaks :/\n")
+
+print("Well anyways...")
+food = input("What's your favorite food? I need recs. ")
+print(food + " sounds gooooood rn\n")
+
+print("Oops! Gotta run.\nNice meeting you " + name + "!")
+print("-" * 40)
