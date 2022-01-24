d9bef4fafc51ffa8394c48d6eee5b2956b855f04
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-24 14:42:03 -0500

    Create main.py

---
+++ [7] Chatbox/main.py
@@ -0,0 +1,14 @@
+UserInput = input("What is your name?")
+print("Nice to meet you, " + UserInput + ".")
+
+UserDay = input("How was your day?")
+print("That's good!")
+
+UserLunch = input("What did you eat for lunch?")
+print("I hate grilled cheese!")
+
+UserHw = input("What do you have for homework?")
+print("that's a lot!")
+
+UserEnd = input("Go do your homework!")
+print("Great " + UserInput)
